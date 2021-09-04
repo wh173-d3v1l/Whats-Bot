2701869d4c89b226fd2417afe9e8153851d1ef36
NzI2OTdhNjE2NA <73955457+riz4d@users.noreply.github.com>
2021-09-04 19:28:38 +0530

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,34 @@
+/*
+# Copyright (C) 2021 rizad.
+#
+# Toxic bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Toxic bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+*/
+
+function successfullMessage(msg) {
+    return "✅ *Toxic Bot*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🛑 *Toxic Bot*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⏺️ *Toxic Bot*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
