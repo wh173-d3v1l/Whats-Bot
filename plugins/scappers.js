4d3ee32c5e6ada6b82a5e756b4318ee7946b253b
NzI2OTdhNjE2NA <73955457+riz4d@users.noreply.github.com>
2021-09-05 11:14:02 +0530

    Update scappers.js

--- plugins/scappers.js
+++ plugins/scappers.js
@@ -1033,6 +1033,37 @@ else if (config.WORKTYPE == 'public') {
             message.reply(Lang.IMG.format((result.length < 2 ? result.length : 2), match[1]));
         });
     })); 
+    Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: Lang.SONG_DESC}, (async (message, match) => { 
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
+    let arama = await yts(match[1]);
+    arama = arama.all;
+    if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
+    var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_SONG,MessageType.text);
+
+    let title = arama[0].title.replace(' ', '+');
+    let stream = ytdl(arama[0].videoId, {
+        quality: 'highestaudio',
+    });
+    
+    got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
+    ffmpeg(stream)
+        .audioBitrate(320)
+        .save('./' + title + '.mp3')
+        .on('end', async () => {
+            const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
+            writer.setFrame('TIT2', arama[0].title)
+                .setFrame('TPE1', [arama[0].author.name])
+                .setFrame('APIC', {
+                    type: 3,
+                    data: fs.readFileSync(title + '.jpg'),
+                    description: arama[0].description
+                });
+            writer.addTag();
+
+            reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
+            await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+        });
+    }));
 
     Asena.addCommand({pattern: 'isong ?(.*)', fromMe: false, desc: Lang.ISONG_DESC}, (async (message, match) => { 
 
