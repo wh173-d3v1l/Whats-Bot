3fe76371e88d42c6694f04f4e178f3cbec48e161
NzI2OTdhNjE2NA <73955457+riz4d@users.noreply.github.com>
2021-09-05 10:56:13 +0530

    Update scappers.js

--- plugins/scappers.js
+++ plugins/scappers.js
@@ -440,7 +440,7 @@ if (config.WORKTYPE == 'private') {
             await message.client.sendMessage(message.jid,buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
         }));
     }
-    Asena.addCommand({pattern: 'song ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (async (message, match) => { 
+    Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: Lang.SONG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
         let arama = await yts(match[1]);
@@ -1034,7 +1034,7 @@ else if (config.WORKTYPE == 'public') {
         });
     })); 
 
-    Asena.addCommand({pattern: 'isong ?(.*)', fromMe: true, desc: Lang.ISONG_DESC}, (async (message, match) => { 
+    Asena.addCommand({pattern: 'isong ?(.*)', fromMe: false, desc: Lang.ISONG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
         let arama = await yts(match[1]);
