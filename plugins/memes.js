a037eebdad234c16b1e1d126774649755f623ba5
NzI2OTdhNjE2NA <73955457+riz4d@users.noreply.github.com>
2021-09-05 14:09:45 +0530

    Add files via upload

---
+++ plugins/memes.js
@@ -0,0 +1,120 @@
+const Asena = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const memeMaker = require('meme-maker')
+const fs = require('fs')
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('memes');
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'meme ?(.*)', fromMe: true, desc: Lang.MEMES_DESC}, (async (message, match) => {   
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var topText, bottomText;
+        if (match[1].includes(';')) {
+            var split = match[1].split(';');
+            topText = split[1];
+            bottomText = split[0];
+        }
+	    else {
+            topText = match[1];
+            bottomText = '';
+        }
+    
+	    var info = await message.reply(Lang.DOWNLOADING);
+	
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        }); 
+    
+	    memeMaker({
+            image: location,         
+            outfile: 'toxic-meme.png',
+            topText: topText,
+            bottomText: bottomText,
+        }, async function(err) {
+            if(err) throw new Error(err)
+            await message.client.sendMessage(message.jid, fs.readFileSync('toxic-meme.png'), MessageType.image, {filename: 'toxic-meme.png', mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ ᴛᴏxɪᴄ ᴍᴡᴏʟ [@ʀɪᴢᴀᴅ__x96]'});
+            await info.delete();    
+        });
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'meme ?(.*)', fromMe: false, desc: Lang.MEMES_DESC}, (async (message, match) => {    
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var topText, bottomText;
+        if (match[1].includes(';')) {
+            var split = match[1].split(';');
+            topText = split[1];
+            bottomText = split[0];
+        }
+	    else {
+            topText = match[1];
+            bottomText = '';
+        }
+    
+	    var info = await message.reply(Lang.DOWNLOADING);
+	
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        }); 
+    
+	    memeMaker({
+            image: location,         
+            outfile: 'toxic-meme.png',
+            topText: topText,
+            bottomText: bottomText,
+        }, async function(err) {
+            if(err) throw new Error(err)
+            await message.client.sendMessage(message.jid, fs.readFileSync('toxic-meme.png'), MessageType.image, {filename: 'toxic-meme.png', mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ ᴛᴏxɪᴄ ᴍᴡᴏʟ [@ʀɪᴢᴀᴅ__x96]'});
+            await info.delete();    
+        });
+    }));
+    Asena.addCommand({pattern: 'meme ?(.*)', fromMe: true, desc: Lang.MEMES_DESC, dontAddCommandList: true}, (async (message, match) => {   
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var topText, bottomText;
+        if (match[1].includes(';')) {
+            var split = match[1].split(';');
+            topText = split[1];
+            bottomText = split[0];
+        }
+	    else {
+            topText = match[1];
+            bottomText = '';
+        }
+    
+	    var info = await message.reply(Lang.DOWNLOADING);
+	
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        }); 
+    
+	    memeMaker({
+            image: location,         
+            outfile: 'toxic-meme.png',
+            topText: topText,
+            bottomText: bottomText,
+        }, async function(err) {
+            if(err) throw new Error(err)
+            await message.client.sendMessage(message.jid, fs.readFileSync('toxic-meme.png'), MessageType.image, {filename: 'toxic-meme.png', mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ ᴛᴏxɪᴄ ᴍᴡᴏʟ [@ʀɪᴢᴀᴅ__x96]'});
+            await info.delete();    
+        });
+    }));
+}
